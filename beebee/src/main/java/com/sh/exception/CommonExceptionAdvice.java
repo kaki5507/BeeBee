0b0f56bc04dd8267b01d3fd25c7b2805e6ea3dab
kaki <ckdbioksh@naver.com>
2021-08-24 23:06:59 +0900

    fix encoding
    - 인코딩 관련 EUC-KR -> UTF-8로 모두 고쳤습니다.
    - 404.jsp파일 실습해보았습니다.

--- beebee/src/main/java/com/sh/exception/CommonExceptionAdvice.java
+++ beebee/src/main/java/com/sh/exception/CommonExceptionAdvice.java
@@ -1,8 +1,11 @@
 package com.sh.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.log4j.Log4j;
 
@@ -10,6 +13,7 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class CommonExceptionAdvice {
 
+	// 모든 예외
 	@ExceptionHandler(Exception.class)
 	public String except(Exception ex, Model model) {
 		
@@ -17,5 +21,13 @@ public class CommonExceptionAdvice {
 		model.addAttribute("exception", ex);
 		log.error(model);
 		return "error_page";
+	}
+	
+	// 404에러
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handle404(NoHandlerFoundException ex) {
+		
+		return "common/custom404";
 	}
 }
