019d0cc30ed83a3e6e4fcdd22aa22c16d7cf456a
kaki5507 <ckdbioksh@naver.com>
2021-10-06 11:34:02 +0900

    bug

---
+++ beebee/bin/src/main/webapp/WEB-INF/db/grant.sql
@@ -0,0 +1,11 @@
+-- beebee 만들기 비밀번호 beebee
+CREATE USER beebee IDENTIFIED BY beebee
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP;
+
+-- 특정 행위 권한,롤(role)이 필요 하므로 GRANT 구문을 이용해서 처리
+GRANT CONNECT, DBA TO beebee;
+
+-- 비밀번호 바꾸기
+alter user beebee IDENTIFIED BY 5507;
+
