e7bef6fa0257fc44418c7277b8a3e5fdebd711f7
kaki5507 <ckdbioksh@naver.com>
2021-10-12 23:28:18 +0900

    Fixed: home change
    - home index 경로 변경
    - localhost 오류 해결 완료 clean

--- beebee/src/main/java/com/sh/controller/HomeController.java
+++ beebee/src/main/java/com/sh/controller/HomeController.java
@@ -1,33 +1,28 @@
 package com.sh.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sh.service.MemberService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 @Controller
+@RequestMapping("/")
+@Log4j
+@AllArgsConstructor
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private MemberService service;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@GetMapping("/home")
+	public String home() {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "main/index";
+		return "/main/index";
 	}
 	
 }
