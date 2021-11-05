0895772cbeaac822eece710303aedd6ae65006af
kaki5507 <ckdbioksh@naver.com>
2021-11-06 00:24:18 +0900

    Fixed : FileUpload prevent
    - 중복 방지 UUID 적용
    - 요일별 폴더 처리

--- beebee/src/main/java/com/sh/controller/UploadController.java
+++ beebee/src/main/java/com/sh/controller/UploadController.java
@@ -1,6 +1,9 @@
 package com.sh.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,11 +22,26 @@ public class UploadController {
 		log.info("upload form");
 	}
 	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+		
+		return str.replace("-", File.separator);
+	}
+	
 	@PostMapping("/uploadAjaxAction")
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
-		log.info("update ajax post ...");
-		
 		String uploadFolder = "D:\\upload";
+		
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path : " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
 		
 		for(MultipartFile multipartFile : uploadFile) {
 			log.info("------------");
@@ -36,7 +54,11 @@ public class UploadController {
 			
 			log.info("only file name : " + uploadFileName);
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
