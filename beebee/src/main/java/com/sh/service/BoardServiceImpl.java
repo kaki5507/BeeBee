83b4431d0db720bdb256395c497b41fa74a9b852
kaki5507 <ckdbioksh@naver.com>
2021-11-16 01:55:26 +0900

    Refactor : Attach deleteAll
    - 수정용 게시글 전부 지우는 메소드 추가
    - 수정을 하기 위해서 먼저 다 지우고, 추가된 파일을 보냄

--- beebee/src/main/java/com/sh/service/BoardServiceImpl.java
+++ beebee/src/main/java/com/sh/service/BoardServiceImpl.java
@@ -52,20 +52,36 @@ public class BoardServiceImpl implements BoardService{
 		return mapper.read(bno);
 	}
 
+	@Transactional
 	@Override
 	public boolean modify(BoardVO board) {
 		
 		log.info("modify....." + board);
 		
-		return mapper.update(board) == 1;
+		attachMapper.deleteAll(board.getBno());
+		
+		boolean modifyResult = mapper.update(board) == 1;
+		
+		if(modifyResult && board.getAttachList() != null && board.getAttachList().size() > 0) {
+			board.getAttachList().forEach(attach ->{
+				attach.setBno(board.getBno());
+				attachMapper.insert(attach);
+			});
+		}
+		
+		return modifyResult;
 	}
 
+	@Transactional
 	@Override
 	public boolean remove(Long bno) {
 		
 		log.info("remove...." + bno);
 		// 댓글 먼저 다 제거 
 		mapper.deleteForeignKey(bno);
+		
+		// 첨부파일 다 제거
+		attachMapper.deleteAll(bno);
 		
 		return mapper.delete(bno) == 1;
 	}
