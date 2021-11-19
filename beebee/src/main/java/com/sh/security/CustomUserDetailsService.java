5cd31a202f14e89311f3c746e60c98cd4c4718e6
kaki5507 <ckdbioksh@naver.com>
2021-11-20 00:13:54 +0900

    Feat : CustomUserDetailsService
    - 커스텀유저디테일 서비스 직접 구현 / 사용자의 정보를 원하는 형식으로 맞게 변형 가능
    - CustomUser를 이용해 MemberVO를 User타입으로 반환 가능

---
+++ beebee/src/main/java/com/sh/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.sh.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.sh.domain.MemberVO;
+import com.sh.mapper.MemberMapper;
+import com.sh.security.domain.CustomUser;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CustomUserDetailsService implements UserDetailsService{
+
+	@Setter(onMethod_ = { @Autowired})
+	private MemberMapper memberMapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		log.warn("Load User By username : " + username);
+		
+		MemberVO vo = memberMapper.read(username);
+		
+		log.warn("queried by member mapper : " + vo);
+		
+		return vo == null ? null : new CustomUser(vo);
+	}
+
+}
